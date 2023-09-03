90519abb43ea34c2f9d4af0b2f8d36089abfd91a
Artem Basalaev <basa62.1996@gmail.com>
2023-09-03 21:57:00 +0200

    Add users table

---
+++ sql/seeds/0_create_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, birthday) VALUES ('Dualboot', '2000-12-12');
+INSERT INTO users (name, birthday) VALUES ('Artem', '1996-09-06');
+INSERT INTO users (name, birthday) VALUES ('Mari', '1996-08-06');
